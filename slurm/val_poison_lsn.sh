8a334e63b2ec1b873968fd8cf4a21f069683aeae
Didanny <daa50@mail.aub.edu>
2025-11-19 14:16:41 -0800

    Update train and val scripts

---
+++ slurm/val_poison_lsn.sh
@@ -0,0 +1,14 @@
+#!/bin/sh 
+#SBATCH -n 9 
+#SBATCH -N 1 
+#SBATCH -p opengpu.p
+#SBATCH -w poison 
+#SBATCH --gres=gpu:4
+#SBATCH -o slurm_logs/log_poison_lanesegnet.out
+#SBATCH -e slurm_logs/err_poison_lanesegnet.out
+
+export PYTHONPATH=$(pwd):$PYTHONPATH
+./tools/dist_test.sh 4 work_dirs/lanesegnet_251115.214225 epoch_4
+./tools/dist_test.sh 4 work_dirs/lanesegnet_251115.214225 epoch_8
+./tools/dist_test.sh 4 work_dirs/lanesegnet_251115.214225 epoch_12
+./tools/dist_test.sh 4 work_dirs/lanesegnet_251115.214225 latest
--- tools/dist_test.sh
+++ tools/dist_test.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 set -x
 
-timestamp=`date +"%y%m%d.%H%M%S"`
-
-WORK_DIR=work_dirs/lanesegnet
+GPUS=$1
+WORK_DIR=$2
+CHECKPOINT_NAME=$3
 CONFIG=projects/configs/lanesegnet_r50_8x1_24e_olv2_subset_A.py
 
-CHECKPOINT=${WORK_DIR}/latest.pth
+# Construct full checkpoint path
+CHECKPOINT=${WORK_DIR}/${CHECKPOINT_NAME}.pth
 
-GPUS=$1
 PORT=${PORT:-28510}
 
 python -m torch.distributed.run --nproc_per_node=$GPUS --master_port=$PORT \
     tools/test.py $CONFIG $CHECKPOINT --launcher pytorch \
-    --out-dir ${WORK_DIR}/test --eval openlane_v2 ${@:2} \
-    2>&1 | tee ${WORK_DIR}/test.${timestamp}.log
+    --out-dir ${WORK_DIR}/test --eval openlane_v2 ${@:4} \
+    2>&1 | tee ${WORK_DIR}/test.${CHECKPOINT_NAME}.log
